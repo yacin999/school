3149371255eb3183c21ac92b67b55301a3a1354a
yacin999 <yacinomar999@gmail.com>
2024-10-21 15:28:20 +0100

    add glass sheet

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import Menu from './menu'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { Logout } from '@/icons'
+import GlassSheet from '@/components/global/glass-sheet'
+import { MenuIcon } from 'lucide-react'
 
 
 
@@ -10,6 +15,31 @@ const LandingPageNavbar = () => {
       <Menu
         orientation={"desktop"}
       />
+      <div className='flex gap-2'>
+        <Link
+          href={"/sign-in"}
+        >
+          <Button
+            variant={"outline"}
+            className='bg-themeBlack rounded-2xl flex gap-2 border-themeGray hover:bg-themeGray'
+          >
+            <Logout/>
+            Login
+          </Button>
+        </Link>
+        <GlassSheet
+          triggerClass='lg:hidden'
+          trigger={
+            <Button
+              variant={"ghost"}
+              className='hover:bg-transparent'
+            >
+              <MenuIcon size={30}/>
+            </Button>
+          }>
+            <Menu orientation='mobile'/>
+        </GlassSheet>
+      </div>
     </div>
   )
 }
