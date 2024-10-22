6bbb9fad4c58e2c7bdad63e1331a661f43a9bae8
yacin999 <yacinomar999@gmail.com>
2024-10-22 08:50:23 +0100

    create dashboard snippet

--- src/app/(landing)/_components/call-to-action/index.tsx
+++ src/app/(landing)/_components/call-to-action/index.tsx
@@ -1,14 +1,39 @@
+import GradientText from '@/components/global/gradient-text'
+import { Button } from '@/components/ui/button'
+import { BadgePlus } from '@/icons'
+import Link from 'next/link'
 import React from 'react'
 
-type Props = {}
 
-const CallToAction = (props: Props) => {
+const CallToAction = () => {
   return (
     <div className='flex flex-col items-start md:items-center gap-y-5 md:gap-y-0'>
         <GradientText 
             className="text-[35px] md:text-[40px] lg:text-[55px] xl:text-[70px] 2xl:text-[80px] leading-tight font-semibold"
             element='H1'
         >Bringing communities <br className='md:hidden'/></GradientText>
+        <p className='text-sm md:text-center text-left text-muted-foreground'>
+          School is a vibrant online community  platform that empowers
+          <br className='md:hidden'/>
+          people to connect, 
+          <br className='md:block'/>{" "}
+          collaborate, and cultivate meaningful
+          <br className='md:hidden'/>
+          relationships
+        </p>
+        <div className='flex md:flex-row flex-col md:justify-center gap-5 md:mt-5 w-full'>
+          <Button 
+            variant={"outline"}
+            className='rounded-xl bg-transparent text-base'
+          >Watch Demo</Button>
+          <Link href={"/sign-in"}>
+            <Button className='rounded-xl text-base flex gap-2 w-full'>
+              <BadgePlus/> Get Started
+            </Button>
+          </Link>
+        </div>
+        
+
     </div>
   )
 }
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,12 @@
+import CallToAction from "./_components/call-to-action";
+import DashboardSnippet from "./_components/dashboard-snippet";
 
 export default function Home() {
   return (
    <main className="md:px-10 flex flex-col gap-36">
     <div>
       <CallToAction/>
+      <DashboardSnippet/>
     </div>
    </main>
   );
