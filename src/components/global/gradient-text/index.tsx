13f501bb1fd25b8b86bca7d92ce7bafe14c322f2
yacin999 <yacinomar999@gmail.com>
2024-10-22 08:11:07 +0100

    create text gradient

--- src/components/global/gradient-text/index.tsx
+++ src/components/global/gradient-text/index.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils'
 import React from 'react'
 
 type GradientTextProps = {
@@ -7,6 +8,21 @@ type GradientTextProps = {
 }
 
 const GradientText = ({element, children, className}: GradientTextProps) => {
+
+  switch (element) {
+    case "H1":
+      return (
+        <h1 className={cn(className, "text-gradient")}>{children}</h1>
+      )
+    case "H2" :
+      return (
+        <h2 className={cn(className, "text-gradient")}>{children}</h2>
+      )
+    default:
+      return (
+        <p className={cn(className, "text-gradient")}>{children}</p>
+      )
+  }
   return (
     <p></p>
   )
