e8f5eaf8f8ab29b1d3bb21fa0b514d99b13d79be
yacin999 <yacinomar999@gmail.com>
2024-10-22 10:46:41 +0100

    complete pricing section

--- src/app/(landing)/_components/pricing/index.tsx
+++ src/app/(landing)/_components/pricing/index.tsx
@@ -1,5 +1,9 @@
 import BackdropGradient from "@/components/global/backdrop-gradient"
 import GradientText from "@/components/global/gradient-text"
+import { Button } from "@/components/ui/button"
+import { Card, CardDescription, CardTitle } from "@/components/ui/card"
+import { Check } from "@/icons"
+import Link from "next/link"
 
 
 
@@ -16,7 +20,49 @@ export const PricingSection = () => {
             >
                 Pricing Plans That Fit Your Right
             </GradientText>
+            <p className="text-sm md:text-center text-left text-muted-foreground">
+                School is vibrant online community platform that empowers poeple to connect, 
+                <br className="hidden md:block"/>
+                collaborate, and cultivate meaningful relationships
+            </p>
         </BackdropGradient>
+
+        <Card className="p-7 mt-10 md:w-auto w-full bg-themeBlack border-themeGray">
+            <div className=" flex flex-col gap-2">
+                <CardTitle>99/mo</CardTitle>
+                <CardDescription className="text-[#B4B0AE]">Great if you&apos;re just getting started</CardDescription>
+                <Link href='#' className="w-full mt-3">
+                    <Button
+                        variant="default"
+                        className="bg-[#333337] w-full rounded-2xl text-white hover:text-[#333337]">
+                            Start for frees
+                        </Button>
+                </Link>
+            </div>
+            <div className="flex flex-col gap-2 text-[#B4B0AE] mt-5">
+                <p>Features</p>
+                <span className="flex gap-2 mt-3 items-center">
+                    <Check/>
+                    Feature number 1
+                </span>
+                <span className="flex gap-2 mt-3 items-center">
+                    <Check/>
+                    Feature number 1
+                </span>
+                <span className="flex gap-2 mt-3 items-center">
+                    <Check/>
+                    Feature number 1
+                </span>
+                <span className="flex gap-2 mt-3 items-center">
+                    <Check/>
+                    Feature number 1
+                </span>
+                <span className="flex gap-2 mt-3 items-center">
+                    <Check/>
+                    Feature number 1
+                </span>
+            </div>
+        </Card>
     </div>
   )
 }
--- src/components/global/backdrop-gradient/index.tsx
+++ src/components/global/backdrop-gradient/index.tsx
@@ -1,19 +1,18 @@
 import { cn } from "@/lib/utils"
-import React from "react"
 
-type props = {
-    children : React.ReactNode
-    className ? : string
-    container ? : string
+type Props = {
+  children: React.ReactNode
+  className?: string
+  container?: string
 }
 
-
-const BackdropGradient = ({children, className, container} : props) => {
+const BackdropGradient = ({ children, className, container }: Props) => {
   return (
     <div className={cn("relative w-full flex flex-col", container)}>
-        <div className={cn("absolute rounded-[50%] radial--blur mx-10", className)}>
-        {children}
-        </div>
+      <div
+        className={cn("absolute rounded-[50%] radial--blur mx-10", className)}
+      />
+      {children}
     </div>
   )
 }
