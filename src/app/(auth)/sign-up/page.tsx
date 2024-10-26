4b0f34444151ed5a04886165eeb5b2d1a6f751c6
yacin999 <yacinomar999@gmail.com>
2024-10-26 16:19:23 +0100

    complate signup page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,13 +1,24 @@
 import SignUpForm from '@/components/forms/sign-up'
+import GoogleAuthButton from '@/components/global/google-oauth-button'
+import { Separator } from '@/components/ui/separator'
 import React from 'react'
 
 
 const SignUpPage = () => {
    
   return (
-    <div>
+    <>
+      <h5 className='font-bold text-base text-themeTextWhite'>Signup</h5>
+      <p className='text-themeTextGray leading-tight'>Network with poeple from around the world, join groups, craete your own, watch courses and become the best of yourself.</p>
       <SignUpForm/>
-    </div>
+      <div className='my-10 w-full relative'>
+        <div className='bg-black p-3 absolute text-themeTextGray text-xs top-1/2 left-1/2 transform translate-x-1/2 translate-y-1/2'>
+          OR CONTINUE WITH
+        </div>
+        <Separator orientation='horizontal' className='bg-themeGray'/>
+        <GoogleAuthButton method='signup'/>
+      </div>
+    </>
   )
 }
 
--- src/app/callback/sign-in/page.tsx
+++ src/app/callback/sign-in/page.tsx
@@ -1,7 +1,6 @@
 import { onSignInUser } from '@/actions/auth'
 import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
-import React from 'react'
 
 
 const CompleteSignIn = async () => {
@@ -16,11 +15,8 @@ const CompleteSignIn = async () => {
         return redirect(`/group/${authenticated.groupId}/channel/${authenticated.channelId}`)
 
     if (authenticated.status !== 200) {
-        redirect("/sign-in")
+        return redirect("/sign-in")
     }
-  return (
-    <div>CompleteSignIn</div>
-  )
 }
 
 export default CompleteSignIn
