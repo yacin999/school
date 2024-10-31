0359542fc7864d4d343e742ac546759a706f5e09
yacin999 <yacinomar999@gmail.com>
2024-10-31 17:58:03 +0100

    add icon dropdown

--- src/components/global/sidebar/icon-dropdown.tsx
+++ src/components/global/sidebar/icon-dropdown.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { DropDown } from '../drop-down'
 import IconRenderer from '../icon-renderer'
+import { cn } from '@/lib/utils'
+import { ICON_LIST } from '@/constants/icons'
 
 type Props = {
     ref : React.RefObject<HTMLButtonElement>
@@ -26,7 +28,21 @@ const IconDropDown = ({ref, icon, page, channelid, currentIcon, onSetIcon}: Prop
             </span>
         }
     >
-
+        <div id='icon-list' className='flex gap-x-2'>
+            {ICON_LIST.map((icons) => icons.icon !== icon && (
+                <span 
+                    key={icons.id}
+                    className={cn(currentIcon === icons.icon ? 'bg-themeGray' : 'p-2 rounded-lg')}
+                    onClick={() => onSetIcon(icons.icon)}
+                >
+                    <IconRenderer
+                        icon={icons.icon}
+                        mode={page === channelid ? "LIGHT" : "DARK"}
+                    />
+                </span>
+            ))
+            }
+        </div>
     </DropDown>
   )
 }
