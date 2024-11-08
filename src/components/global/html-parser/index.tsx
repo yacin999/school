81d9f64c77055433cf3afec008e2ed1c668db092
yacin999 <yacinomar999@gmail.com>
2024-11-08 20:54:13 +0100

    update rich text editor

--- src/components/global/html-parser/index.tsx
+++ src/components/global/html-parser/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
     html : string
 }
 
 const HtmlParser = ({html}: Props) => {
+
+    const [mounted, setMounted] = useState<boolean>(false)
+
+    useEffect(()=>{
+        setMounted
+    }, [])
   return (
     <div>HtmlParser</div>
   )
