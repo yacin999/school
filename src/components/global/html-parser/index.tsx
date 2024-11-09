009b014fb9c0e6417cc201c1b801710c92b3c54a
yacin999 <yacinomar999@gmail.com>
2024-11-09 20:54:19 +0100

    add html react parser

--- src/components/global/html-parser/index.tsx
+++ src/components/global/html-parser/index.tsx
@@ -9,10 +9,13 @@ const HtmlParser = ({html}: Props) => {
     const [mounted, setMounted] = useState<boolean>(false)
 
     useEffect(()=>{
-        setMounted
+        setMounted(true)
+        return () => setMounted(true)
     }, [])
   return (
-    <div>HtmlParser</div>
+    <div className='[&_h1]:text-4xl [&_h2]:text-3xl [&_blockquote]:italic [&_iframe]:aspect-video [&_h3]:text-2xl text-themeTextGray flex flex-col gap-y-3'>
+      {mounted && parse()}
+    </div>
   )
 }
 
