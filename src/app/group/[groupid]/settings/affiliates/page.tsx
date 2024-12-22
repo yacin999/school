5b674f324d1091040e9babcf9908dd22d18c5724
yacin999 <yacinomar999@gmail.com>
2024-12-22 20:28:14 +0100

    add onGetAffiliateLink action

--- src/app/group/[groupid]/settings/affiliates/page.tsx
+++ src/app/group/[groupid]/settings/affiliates/page.tsx
@@ -1,4 +1,5 @@
 import { onGetAffiliateLink } from "@/actions/groups"
+import { CopyButton } from "@/components/global/copy-button"
 import { Card, CardDescription, CardTitle } from "@/components/ui/card"
 
 type Props = {
